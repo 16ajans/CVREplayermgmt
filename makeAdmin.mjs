0e32e1fea3f0774631aa8863eff75f916b3537e3
Alex Jansen <alexjansen210@gmail.com>
2021-02-27 01:35:33 -0600

    housekeeping + admin things

--- makeAdmin.mjs
+++ makeAdmin.mjs
@@ -3,20 +3,34 @@ import admin from 'firebase-admin';
 admin.initializeApp();
 
 var email = process.argv.slice(2)[0];
+var make = (process.argv.slice(2)[1] === 'true');
 
-if (email) {
-
+if (make) {
   admin
     .auth()
     .getUserByEmail(email)
     .then((userRecord) => {
-      admin.auth().setCustomUserClaims(userRecord.uid, { admin: true })
-      console.log("Set sucessfully.");
+      admin.auth().setCustomUserClaims(userRecord.uid, { admin: true }).then((lmaoWut) => {
+        console.log("Set as admin sucessfully.");
+        process.exit(0);
+      });
     })
     .catch((error) => {
       console.log('Error fetching user data:', error);
+      process.exit(1);
     });
-
 } else {
-  console.log("Provide an email of user to be made into admin.")
+  admin
+    .auth()
+    .getUserByEmail(email)
+    .then((userRecord) => {
+      admin.auth().setCustomUserClaims(userRecord.uid, { admin: false }).then((lmaoWut) => {
+        console.log("Removed as admin.");
+        process.exit(0);
+      });
+    })
+    .catch((error) => {
+      console.log('Error fetching user data:', error);
+      process.exit(1);
+    });
 }
